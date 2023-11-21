23f280a4395a1fa544131281248fb8133e0db19e
Kathy Ho <kathyho04@gmail.com>
2023-11-21 15:44:13 -0500

    added storage permissions (for some reason popup not showing...?)

--- app/src/main/java/com/example/objtranslator/MainActivity.java
+++ app/src/main/java/com/example/objtranslator/MainActivity.java
@@ -7,8 +7,7 @@ import android.os.Bundle;
 import com.example.objtranslator.helpers.ImageHelperActivity;
 
 import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
 import android.view.View;
 
 import android.widget.EditText;
--- app/src/main/java/com/example/objtranslator/helpers/ImageHelperActivity.java
+++ app/src/main/java/com/example/objtranslator/helpers/ImageHelperActivity.java
@@ -2,7 +2,12 @@ package com.example.objtranslator.helpers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.content.pm.PackageManager;
+import androidx.annotation.NonNull;
+import android.Manifest;
+import android.util.Log;
 
 import com.example.objtranslator.R;
 
@@ -12,5 +17,19 @@ public class ImageHelperActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_helper);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                    PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d(ImageHelperActivity.class.getSimpleName(), "grant result for " + permissions[0] + " is " + grantResults[0]);
     }
 }
