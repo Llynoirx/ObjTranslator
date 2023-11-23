85ab6f8ccb8c8abf31b2002797fc8d032051758f
Kathy Ho <kathyho04@gmail.com>
2023-11-23 07:04:53 -0500

    added functionality for onPickImage button + fixed UI for ImageHelperActivity

--- app/src/main/java/com/example/objtranslator/helpers/ImageHelperActivity.java
+++ app/src/main/java/com/example/objtranslator/helpers/ImageHelperActivity.java
@@ -2,21 +2,32 @@ package com.example.objtranslator.helpers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.content.pm.PackageManager;
 import androidx.annotation.NonNull;
 import android.Manifest;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.objtranslator.R;
 
 public class ImageHelperActivity extends AppCompatActivity {
 
+    private int REQUEST_PICK_IMG = 1000;
+    private ImageView inputImageView;
+    private TextView outputTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_helper);
+
+        inputImageView = findViewById(R.id.imageViewInput);
+        outputTextView = findViewById(R.id.textViewOutput);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !=
@@ -32,4 +43,16 @@ public class ImageHelperActivity extends AppCompatActivity {
 
         Log.d(ImageHelperActivity.class.getSimpleName(), "grant result for " + permissions[0] + " is " + grantResults[0]);
     }
+
+    public void onPickImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+
+        startActivityForResult(intent, REQUEST_PICK_IMG);
+    }
+
+    public void onStartCamera(View view) {
+
+    }
+
 }
