05bc1b02ef7779e3014ed1d567532867fd38ebd9
Kathy Ho <kathyho04@gmail.com>
2023-11-23 08:35:42 -0500

    added image display (untested since emulator not working)

--- app/src/main/java/com/example/objtranslator/helpers/ImageHelperActivity.java
+++ app/src/main/java/com/example/objtranslator/helpers/ImageHelperActivity.java
@@ -1,19 +1,26 @@
 package com.example.objtranslator.helpers;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.content.pm.PackageManager;
 import androidx.annotation.NonNull;
 import android.Manifest;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.objtranslator.R;
+
+import java.io.IOException;
 
 public class ImageHelperActivity extends AppCompatActivity {
 
@@ -55,4 +62,31 @@ public class ImageHelperActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Display image once user selects it
+        if(resultCode==RESULT_OK && requestCode==REQUEST_PICK_IMG) {
+            Uri uri = data.getData();
+            Bitmap bitmap = loadFromUri(uri);
+            inputImageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private Bitmap loadFromUri(Uri uri){
+        Bitmap bitmap = null;
+
+        try {
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O_MR1){
+                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), uri);
+                bitmap = ImageDecoder.decodeBitmap(source);
+            } else {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return bitmap;
+    }
 }
