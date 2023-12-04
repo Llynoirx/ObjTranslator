32ee98568cf7a8477a105ab5da5365eaa7c06523
llynoirx <kathyho04@gmail.com>
2023-12-04 09:48:27 -0500

    check if password and email are valid

--- app/src/main/java/com/example/objtranslator/onboarding/RegisterActivity.java
+++ app/src/main/java/com/example/objtranslator/onboarding/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.objtranslator.onboarding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -13,9 +15,9 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText name, email, password;
-    Button signupBtn;
-    TextView haveAccount;
+    EditText mName, mEmail, mPassword, mPassword2;
+    Button mSignupBtn;
+    TextView mGotoLogin;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
 
@@ -25,13 +27,42 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         //Set variables
-        name = findViewById(R.id.name);
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        signupBtn = findViewById(R.id.signupBtn);
-        haveAccount = findViewById(R.id.haveAccount);
+        mName = findViewById(R.id.name);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mPassword2 = findViewById(R.id.password2);
+        mSignupBtn = findViewById(R.id.signupBtn);
+        mGotoLogin = findViewById(R.id.haveAccount);
 
         fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
+
+        mSignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String password2 = mPassword2.getText().toString().trim();
+
+                //Check if email and password are valid
+                if(TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+                if(password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                if(!password.equals(password2)) {
+                    mPassword2.setError("Password doesn't match");
+                    return;
+                }
+
+            }
+        }
     }
 }
