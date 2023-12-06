edfd9491ba35d5f9f7ba5c2301f7c20ca337e8b6
llynoirx <kathyho04@gmail.com>
2023-12-06 05:20:28 -0500

    register user in firebase for signup and then bring user to homepage

--- app/src/main/java/com/example/objtranslator/onboarding/RegisterActivity.java
+++ app/src/main/java/com/example/objtranslator/onboarding/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.objtranslator.onboarding;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -9,8 +11,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.objtranslator.R;
+import com.example.objtranslator.main.MainActivity;
+import com.example.objtranslator.main.ObjectTranslatorActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -37,14 +45,24 @@ public class RegisterActivity extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
+        if(fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
         mSignupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                String name = mName.getText().toString().trim();
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
                 String password2 = mPassword2.getText().toString().trim();
 
-                //Check if email and password are valid
+                //Check if inputs are valid
+                if(TextUtils.isEmpty(name)) {
+                    mName.setError("Name is Required.");
+                    return;
+                }
                 if(TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required.");
                     return;
@@ -57,12 +75,33 @@ public class RegisterActivity extends AppCompatActivity {
                     mPassword.setError("Password Must be >= 6 Characters");
                     return;
                 }
+                if(TextUtils.isEmpty(password2)) {
+                    mPassword2.setError("Confirm your password");
+                    return;
+                }
                 if(!password.equals(password2)) {
-                    mPassword2.setError("Password doesn't match");
+                    mPassword2.setError("Passwords don't match");
                     return;
                 }
 
-            }
+                //Input is valid, register user in firebase
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "User Created.", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                } else {
+                                    Toast.makeText(RegisterActivity.this,
+                                                   "Error! " + task.getException().getMessage(),
+                                                    Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                    });
+                }
+            });
         }
-    }
 }
