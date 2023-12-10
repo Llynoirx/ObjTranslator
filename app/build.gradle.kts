67fa067e87dcc53fbd440b7254525f727fef2763
llynoirx <kathyho04@gmail.com>
2023-12-10 15:09:45 -0500

    added custom model dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,17 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        mlModelBinding = true
+    }
+
+    aaptOptions {
+        noCompress("tflite")
     }
 }
 
@@ -44,7 +52,12 @@ dependencies {
     implementation("com.google.mlkit:object-detection:16.2.6")
     //text translation
     implementation("com.google.mlkit:translate:17.0.1")
+    //log-in/register
     implementation("com.google.firebase:firebase-auth:22.3.0")
+    //custom obj detector
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
+    implementation("com.google.mlkit:object-detection-custom:17.0.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
