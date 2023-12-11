9d8d92edc4caebbea797ed41d1df2765367da1fc
llynoirx <kathyho04@gmail.com>
2023-12-10 19:04:24 -0500

    reverted back

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,20 +26,9 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    buildFeatures {
-        mlModelBinding = true
-    }
-
-    buildFeatures {
-        viewBinding = true
-    }
-    androidResources {
-        noCompress("tflite")
     }
 }
 
@@ -55,19 +44,8 @@ dependencies {
     implementation("com.google.mlkit:object-detection:16.2.6")
     //text translation
     implementation("com.google.mlkit:translate:17.0.1")
-    //log-in/register
+    //sign in
     implementation("com.google.firebase:firebase-auth:22.3.0")
-    //custom obj detector
-    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
-    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
-    implementation("com.google.mlkit:object-detection-custom:17.0.1")
-
-    implementation("org.tensorflow:tensorflow-lite-task-vision:0.2.0")
-    // Import the GPU delegate plugin Library for GPU inference
-    implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:0.4.0")
-    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
-    implementation("com.google.firebase:firebase-ml-model-interpreter:18.0.0")
-    implementation("org.tensorflow:tensorflow-lite:1.13.1@aar")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
